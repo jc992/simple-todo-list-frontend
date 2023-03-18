c169d5231c4df846a466fd4fec5c47cbcd71018a
ffmcgee <51971598+jc992@users.noreply.github.com>
2023-03-18 17:40:16 +0530

    feat: setup Context API for state management (#2)

--- src/context/todoContext.tsx
+++ src/context/todoContext.tsx
@@ -1,11 +1,36 @@
-import { createContext, FC } from 'react';
-import { TodoContextType, TodoProps } from '../@types/todo';
+import React, { createContext, FC, ReactNode } from 'react';
+import { TodoContextType, ITodo, TodoProps } from '../@types/todo';
 
 export const TodoContext = createContext<TodoContextType | null>(null);
 
 const TodoProvider: FC<TodoProps> = ({ children }) => {
+  const [todos, setTodos] = React.useState<ITodo[]>([
+    {
+      id: crypto.randomUUID(),
+      description: 'Learn Javascript',
+      timestamp: new Date().getTime(),
+      isComplete: false,
+    },
+  ]);
+
+  const saveTodo = (todo: ITodo): void => {
+    // TODO implement
+  };
+
+  const updateTodo = (id: string): void => {
+    // TODO implement
+  };
+
+  const deleteTodo = (id: string): void => {
+    // TODO implement
+  };
+
+  const editTodo = (id: string, description: string): void => {
+    // TODO implement
+  };
+
   return (
-    <TodoContext.Provider value={{}}>
+    <TodoContext.Provider value={{ todos, saveTodo, updateTodo, deleteTodo, editTodo }}>
       {children}
     </TodoContext.Provider>
   );
