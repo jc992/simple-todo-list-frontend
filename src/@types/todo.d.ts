47bcd2e93a4c82b9e90840e4210957c575e85879
ffmcgee <jc.992@hotmail.com>
2023-03-18 12:18:43 +0000

    feat: setup Context API for state management

--- src/@types/todo.d.ts
+++ src/@types/todo.d.ts
@@ -1,7 +1,29 @@
 import { ReactNode } from 'react';
 
-export type TodoContextType = {};
+export interface ITodo {
+  id: string;
+  description: string;
+  timestamp: number;
+  isComplete: boolean;
+}
+
+export type TodoContextType = {
+  todos: ITodo[];
+  saveTodo: (todo: ITodo) => void;
+  updateTodo: (id: string) => void;
+  deleteTodo: (id: string) => void;
+  editTodo: (id: string, description: string) => void;
+};
 
 export type TodoProps = {
   children: ReactNode;
-};
+}
+
+export type TodoItemProps = {
+  todo: ITodo;
+  updateTodo: (id: string) => void;
+  deleteTodo: (id: string) => void;
+  editTodo: (id: string, description: string) => void;
+}
+
+export type SortBy = 'asc' | 'desc' | 'timestamp';
