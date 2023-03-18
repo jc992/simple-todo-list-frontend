18703b024eab02ccae4e309b6b2f7c233d15e75c
ffmcgee <51971598+jc992@users.noreply.github.com>
2023-03-18 18:02:30 +0530

    feat: implement save, update, delete and edit item logic (#4)

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,21 +1,49 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { TodoItemProps } from '../@types/todo';
 
 const TodoItem: FC<TodoItemProps> = ({ todo, updateTodo, deleteTodo, editTodo }) => {
+  const [isEditing, setEditing] = useState(false);
+  const [description, setDescription] = useState('');
+
+  const toggleEdit = () => {
+    setDescription(todo.description);
+    setEditing(true);
+  };
+
+  const handleEdit = () => {
+    editTodo(todo.id, description);
+    setEditing(false);
+  };
+
   return (
-    <div className="item-container">
+    <div className="item-container" style={{ display: 'flex', justifyContent: 'space-between' }}>
       <div className="inner-wrapper">
         <div className="checkbox-container">
-          <input className="checkbox" type="checkbox"/>
+          {!isEditing ? (
+            <input
+              className="checkbox"
+              type="checkbox"
+              checked={todo.isComplete}
+              onChange={() => updateTodo(todo.id)}
+            />
+          ) : null}
         </div>
         <div className="text-container">
-          <h1>{todo.description}</h1>
+          {isEditing ? (
+            <input value={description} onChange={(e) => setDescription(e.target.value)}></input>
+          ) : (
+            <h1>{todo.description}</h1>
+          )}
         </div>
       </div>
       <div className="inner-wrapper">
         <div className="button-container">
-          <button>Edit</button>
-          <button>Delete</button>
+          {!isEditing ? (
+            <button onClick={() => toggleEdit()}>Edit</button>
+          ) : (
+            <button onClick={() => handleEdit()}>Change</button>
+          )}
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
         </div>
       </div>
     </div>
--- src/containers/Todos.tsx
+++ src/containers/Todos.tsx
@@ -1,11 +1,12 @@
 import { FC, useContext } from 'react';
-import { TodoContextType, ITodo } from '../@types/todo';
+import { TodoContextType, ITodo, SortBy } from '../@types/todo';
 import { TodoContext } from '../context/todoContext';
 import TodoItem from '../components/TodoItem';
 import Divider from '../components/Divider';
 
 const Todos: FC = () => {
   const { todos, updateTodo, deleteTodo, editTodo } = useContext(TodoContext) as TodoContextType;
+
   return (
     <>
       <h1 className="sort">Todos</h1>
@@ -17,7 +18,7 @@ const Todos: FC = () => {
       <div className="hide-wrapper">
         <h1>Hide Completed</h1>
         <div className="hide-checkbox-container">
-          <input className="hide-checkbox" type="checkbox"/>
+          <input className="hide-checkbox" type="checkbox" />
         </div>
       </div>
     </>
