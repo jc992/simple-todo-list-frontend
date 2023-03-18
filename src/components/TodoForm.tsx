bd5a03be878aead3f03f19612044ede474b5dc2f
ffmcgee <jc.992@hotmail.com>
2023-03-18 12:41:02 +0000

    feat: implement save, update, delete and edit item logic

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,12 +1,31 @@
-import { FC } from 'react';
+import React, { FC, FormEvent } from 'react';
+import { TodoContext } from '../context/todoContext';
+import { TodoContextType, ITodo } from '../@types/todo';
 
 const TodoForm: FC = () => {
+  const { saveTodo } = React.useContext(TodoContext) as TodoContextType;
+  const [formData, setFormData] = React.useState<ITodo | {}>();
+
+  const handleForm = (e: FormEvent<HTMLInputElement>): void => {
+    // TODO fix bug with empty input
+    setFormData({
+      ...formData,
+      [e.currentTarget.id]: e.currentTarget.value,
+    });
+  };
+
+  const handleSaveTodo = (e: React.FormEvent, formData: ITodo | any) => {
+    e.preventDefault();
+    saveTodo(formData);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={(e) => handleSaveTodo(e, formData)}>
       <div className="input-container">
         <div className="input-inner-wrapper">
           <input
             placeholder="  Write new task here..."
+            onChange={handleForm}
             type="text"
             id="description"
             className="input-box"
@@ -14,7 +33,7 @@ const TodoForm: FC = () => {
         </div>
       </div>
       <div className="button-container">
-        <button>Create</button>
+        <button disabled={formData === undefined ? true : false}>Create</button>
       </div>
     </form>
   );
--- src/context/todoContext.tsx
+++ src/context/todoContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, FC, ReactNode } from 'react';
+import React, { createContext, FC } from 'react';
 import { TodoContextType, ITodo, TodoProps } from '../@types/todo';
 
 export const TodoContext = createContext<TodoContextType | null>(null);
@@ -14,19 +14,42 @@ const TodoProvider: FC<TodoProps> = ({ children }) => {
   ]);
 
   const saveTodo = (todo: ITodo): void => {
-    // TODO implement
+    const newTodo: ITodo = {
+      id: crypto.randomUUID(),
+      description: todo.description,
+      timestamp: new Date().getTime(),
+      isComplete: false,
+    };
+    setTodos([...todos, newTodo]);
   };
 
   const updateTodo = (id: string): void => {
-    // TODO implement
+    // TODO refactor this function;
+    todos.filter((todo: ITodo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+        setTodos([...todos]);
+      }
+    });
   };
 
   const deleteTodo = (id: string): void => {
-    // TODO implement
+    const item = todos.find((t) => t.id === id) as ITodo;
+    if (!item) {
+      return;
+    }
+    todos.splice(todos.indexOf(item), 1);
+    setTodos([...todos]);
   };
 
   const editTodo = (id: string, description: string): void => {
-    // TODO implement
+    // TODO refactor this function;
+    todos.filter((todo: ITodo) => {
+      if (todo.id === id) {
+        todo.description = description;
+        setTodos([...todos]);
+      }
+    });
   };
 
   return (
