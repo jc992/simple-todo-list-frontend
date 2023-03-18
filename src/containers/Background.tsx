7b2ad2a530d94f49b61cbca32951767fed704541
ffmcgee <jc.992@hotmail.com>
2023-03-18 13:30:11 +0000

    fix: fix background bug on app start up

--- src/containers/Background.tsx
+++ src/containers/Background.tsx
@@ -2,13 +2,7 @@ import { FC } from 'react';
 import './Background.styles.css';
 
 const Background: FC = () => {
-  return (
-    <>
-      <div id="stars" />
-      <div id="stars2" />
-      <div id="stars3" />
-    </>
-  );
+  return <div id="stars" />;
 };
 
 export default Background;
